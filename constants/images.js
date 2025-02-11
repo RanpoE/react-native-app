137148a305324447793f1f436a5bd8e781a24c2f
Edward <edward.nayve@boom.ai>
2025-02-11 22:04:17 +0800

    Add homescreen

--- constants/images.js
+++ constants/images.js
@@ -3,6 +3,7 @@ import cards from "../assets/images/cards.png"
 import empty from "../assets/images/empty.png"
 import profile from "../assets/images/profile.png"
 import path from "../assets/images/path.png"
+import logo_small from "../assets/images/logo-small.png"
 
 
-export default { logo, cards, empty, profile, path }
+export default { logo, cards, empty, profile, path, logo_small }
